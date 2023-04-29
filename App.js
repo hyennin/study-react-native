767a48930015948b8215155efd9cf9c41b35d526
hyennin <chm050202@gmail.com>
2023-04-29 22:05:41 +0900

    mypage 구현

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MyPage from './src/pages/mypage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native 연습</Text>
-      <StatusBar style="auto" />
+      <MyPage/>
     </View>
   );
 }
@@ -13,8 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginLeft: 16,
+    marginRight: 16
   },
 });
---
+++ src/pages/mypage.js
@@ -0,0 +1,136 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import profile from '../assets/profile.png';
+import settings from '../assets/settings.png';
+import move from '../assets/move.png';
+import cup from '../assets/cup.png';
+import chat from '../assets/chat.png';
+
+const MyPage = () => {
+  return (
+    <View style={styles.container}>
+        <View style={styles.box1}>
+          <View style={styles.textBox}>
+            <Image source={profile}/>
+            <View style={styles.infoBox}>
+                <Text style={styles.grade}>학년 반 번호</Text>
+                <Text style={styles.name}>이름</Text>
+            </View>
+          </View>
+          <TouchableOpacity>
+            <Image source={settings}/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.box2}>
+          <View style={styles.contentBox}>
+            <TouchableOpacity style={styles.competition}>
+              <View style={styles.textBox}>
+                <Image source={cup}/>
+                <Text style={styles.text}>공모전</Text>
+              </View>
+                <Image source={move}/>
+            </TouchableOpacity>
+            <View style={styles.hr}/>
+            <TouchableOpacity style={styles.community}>
+              <View style={styles.textBox}>
+                <Image source={chat}/>
+                <Text style={styles.text}>커뮤니티</Text>
+              </View>
+                <Image source={move}/>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.box3}>
+            <Text style={styles.text2}>도서대출증</Text>
+            <Image source={move}/>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box1: {
+    width: '100%',
+    height: 100,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: -200,
+    borderWidth: 1,
+    borderColor: '#E9E9E9',
+    borderRadius: 16
+  },
+  textBox: {
+    flexDirection: 'row',
+  },
+  infoBox: {
+    marginLeft: 16
+  }, 
+  grade: {
+    fontSize: 12,
+    color: '#8A8A8A'
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 500
+  },
+  box2: {
+    width: '100%',
+    height: 144,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 24,
+    borderWidth: 1,
+    borderColor: '#E9E9E9',
+    borderRadius: 16
+  },
+  contentBox: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  },
+  hr: {
+    width: '90%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#E9E9E9'
+  },
+  competition: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  community: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  box3: {
+    width: '100%',
+    height: 72,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 24,
+    borderWidth: 1,
+    borderColor: '#E9E9E9',
+    borderRadius: 16
+  },
+  text: {
+    marginLeft: 16,
+    fontSize: 16,
+    fontWeight: 500
+  },
+  text2: {
+    fontSize: 16,
+    fontWeight: 500
+  }
+});
+
+export default MyPage;

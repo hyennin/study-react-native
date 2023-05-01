fa15c08fc1ba6f63c831e58b22ea4c61350991c3
hyennin <chm050202@gmail.com>
2023-05-01 16:40:52 +0900

    코드 정리

--- src/pages/councilTab.js
+++ src/pages/councilTab.js
@@ -23,48 +23,6 @@ const styles = StyleSheet.create({
         paddingBottom: 40,
         backgroundColor: 'rgba(255, 255, 255, 1)'
     },
-	tab: {
-		width: '100%',
-        marginTop: 120,
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-	},
-    council: {
-        width: 114,
-        height: 44,
-        alignItems: 'center'
-    },
-    calendar: {
-        width: 114,
-        height: 44,
-        alignItems: 'center'
-    },
-    map: {
-        width: 114,
-        height: 44,
-        alignItems: 'center'
-    },
-    selectedText: {
-        fontSize: 14,
-        fontWeight: 500
-    },
-    text: {
-        fontSize: 14,
-        fontWeight: 400,
-        color: 'rgba(138, 138, 138, 1)'
-    },
-    selectedHr: {
-        width: '108%',
-        marginTop: 16,
-        borderBottomWidth: 2,
-        borderBottomColor: 'rgba(23, 227, 129, 1)'
-    },
-    hr: {
-        width: '108%',
-        marginTop: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(233, 233, 233, 1)'
-    },
     scroll: {
         marginTop: 23
     }
--- src/pages/mapTab.js
+++ src/pages/mapTab.js
@@ -14,48 +14,6 @@ const styles = StyleSheet.create({
         paddingRight: 16,
         paddingBottom: 40,
         backgroundColor: 'rgba(255, 255, 255, 1)'
-    },
-	tab: {
-		width: '100%',
-        marginTop: 120,
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-	},
-    council: {
-        width: 114,
-        height: 44,
-        alignItems: 'center'
-    },
-    calendar: {
-        width: 114,
-        height: 44,
-        alignItems: 'center'
-    },
-    map: {
-        width: 114,
-        height: 44,
-        alignItems: 'center'
-    },
-    selectedText: {
-        fontSize: 14,
-        fontWeight: 500
-    },
-    text: {
-        fontSize: 14,
-        fontWeight: 400,
-        color: 'rgba(138, 138, 138, 1)'
-    },
-    selectedHr: {
-        width: '108%',
-        marginTop: 16,
-        borderBottomWidth: 2,
-        borderBottomColor: 'rgba(23, 227, 129, 1)'
-    },
-    hr: {
-        width: '108%',
-        marginTop: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(233, 233, 233, 1)'
     }
 });
 

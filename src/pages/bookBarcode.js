37d55c0dace264aba03d8db72a266b1143e9729a
hyennin <chm050202@gmail.com>
2023-05-06 20:45:18 +0900

    bookBarcode ui 수정

--- src/pages/bookBarcode.js
+++ src/pages/bookBarcode.js
@@ -1,29 +1,47 @@
 import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import barcode from '../assets/barcode.png';
 import save from '../assets/save.png';
+import saved from '../assets/saved.png';
+import { useState } from 'react';
 
 const BookBarcode = () => {
-  return (
-    <View style={styles.background}>
-        <View style={styles.container}>
-            <View style={styles.box}>
-                <Text style={styles.text}>0학년 0반 0번 김00</Text>
-                <Image source={barcode} style={styles.barcode}/>
-                <TouchableOpacity style={styles.button}>
-                    <Text>이미지 저장</Text>
-                    <Image source={save}/>
-                </TouchableOpacity>
+    const [isHovering, setIsHovering] = useState(0);
+
+    return (
+        <View style={styles.background}>
+            <View style={styles.container}>
+                <View style={styles.box}>
+                    <Text style={styles.text}>0학년 0반 0번 김00</Text>
+                    <Image source={barcode} style={styles.barcode}/>
+                    <Text>S210282</Text>
+                    <TouchableOpacity
+                        activeOpacity={1}
+                        onPressIn={() => setIsHovering(1)}
+                        onPressOut={() => setIsHovering(0)}
+                        style={isHovering ? styles.pressedButton : styles.button}
+                    >
+                        <Text
+                            onPressIn={() => setIsHovering(1)}
+                            onPressOut={() => setIsHovering(0)}
+                            style={isHovering ? styles.pressedText : ''}
+                        >이미지 저장</Text>
+                        <Image 
+                            onPressIn={() => setIsHovering(1)}
+                            onPressOut={() => setIsHovering(0)}
+                            source={isHovering ? saved : save}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
     background: {
         flex: 1,
         justifyContent: 'flex-end',
-        backgroundColor: 'rgba(23, 227, 129, 0.15)'
+        backgroundColor: 'rgba(23, 227, 129, 1)'
     },
     container: {
         width: '100%',
@@ -62,6 +80,20 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'rgba(138, 138, 138, 1)',
         borderRadius: 12
+    },
+    pressedButton: {
+        width: 226,
+        height: 52,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 16,
+        borderWidth: 1,
+        borderColor: 'rgba(23, 227, 129, 1)',
+        borderRadius: 12
+    },
+    pressedText: {
+        color: 'rgba(23, 227, 129, 1)'
     }
 });
 

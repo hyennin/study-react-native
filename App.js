3e2edd7cd19b7699c3558814829ba73664dd3754
hyennin <chm050202@gmail.com>
2023-05-21 21:11:43 +0900

    picker

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import MyStack from './navigation/StackNavigator';
 import Council from './src/pages/councilPage';
 import School from './src/pages/schoolPage';
+import Competition from './src/pages/competition';
 
 export default function App() {
   return (
-    <School/>
+    <Competition/>
   );
 }
 
--- src/pages/competition.js
+++ src/pages/competition.js
@@ -1,10 +1,29 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 import CompetitionCard from '../components/competitionCard'
 import { ScrollView } from 'react-native-gesture-handler';
+import { Picker } from "@react-native-picker/picker";
+import { useState } from 'react';
+import controls from '../assets/controls.png';
 
 const Competition = () => {
+  const [country, setCountry] = useState('Unknown');
+  const picker = <Text><Image source={controls}/>인기순</Text>;
+
   return (
     <View style={styles.container}>
+      <Picker
+        selectedValue={country}
+        onValueChange={(value, index) => setCountry(value)}
+        mode="dropdown" // Android only
+        style={styles.picker}
+      >
+        <Picker.Item label={picker} value="Unknown" />
+        <Picker.Item label="최신순" value="Australia" />
+        <Picker.Item label="Belgium" value="Belgium" />
+        <Picker.Item label="Canada" value="Canada" />
+        <Picker.Item label="India" value="India" />
+        <Picker.Item label="Japan" value="Japan" />
+      </Picker>
       <ScrollView style={styles.scroll}>
         <View style={styles.cardContainer}>
           <CompetitionCard/>
